11eb1d74104ea6a17740fd19701db1a061186221
Kaden <Cartwrightkaden@gmail.com>
2023-03-16 16:26:30 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# load api response into data frame
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# display the df
+streamlit.dataframe(fruityvice_normalized)
